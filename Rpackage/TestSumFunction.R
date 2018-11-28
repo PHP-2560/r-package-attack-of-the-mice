4c9a5cb28b5e998569210c020f0e949850ed1e87
Kirsten Voorhies <kirsten_voorhies@brown.edu>
2018-11-28 14:34:03 -0500

    Updated sumTest

--- Rpackage/TestSumFunction.R
+++ Rpackage/TestSumFunction.R
@@ -1,3 +1,15 @@
+
+#' A Sum Test Function
+#'
+#' This function allows you to express your love of cats.
+#' @param x first value to add together
+#' @param y second value to add together
+#' @keywords sums
+#' @export
+#' @examples
+#' @return x+y 
+#' sumTest()
+
 sumTest <- function(x, y){
   return(x+y)
 }
