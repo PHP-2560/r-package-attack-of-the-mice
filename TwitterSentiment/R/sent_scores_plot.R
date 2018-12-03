5d4b2a98d754e0016b4e7cc66551313798a8664f
Camilla Calmasini <camilla_calmasini@brown.edu>
2018-12-03 12:42:27 -0500

    Added function that creates a graph of total sentiment scores

---
+++ TwitterSentiment/R/sent_scores_plot.R
@@ -0,0 +1,17 @@
+sent_scores_plot = function(data, lexicon){
+  word_counts_nrc = data %>%
+  inner_join(get_sentiments(lexicon)) %>%
+  #Count by word and sentiment
+  count(word, sentiment)
+  sent = word_counts_nrc %>%
+    group_by(sentiment) %>%
+    count()
+  ggplot(sent, aes(sentiment, nn, fill = sentiment)) +
+    geom_col() +
+    xlab("Sentiment") +
+    ylab("Number") +
+    ggtitle("Total sentiment scores")
+}
+
+
+
