26fb775d0387efa8dfd61e8f501af30fa10130a8
Camilla Calmasini <camilla_calmasini@brown.edu>
2018-12-09 21:52:41 -0500

    Updated app with word cloud

--- Shiny App/most_common_words.R
+++ Shiny App/most_common_words.R
@@ -9,8 +9,8 @@ most_common_words = function(data, num_words) {
   mutate(word = reorder(word, n)) %>%
   top_n(num_words)%>%
   ggplot(aes(word, n)) +
-  geom_col(fill = "indianred3") +
-  labs(title = paste("Top", num_words, "Most Commonly Used Words"), x = "Word", y = "Number of Times Used") +
-  theme(axis.title.x = element_text(face = "bold"), axis.title.y = element_blank(), plot.title = element_text(face = "bold", hjust=0.5)) +
-  coord_flip()
+    geom_col(fill = "indianred3") +
+    labs(title = paste("Top", num_words, "Most Commonly Used Words"), x = "Word", y = "Number of Times Used") +
+    theme(axis.title.x = element_text(face = "bold"), axis.title.y = element_blank(), plot.title = element_text(face = "bold", hjust=0.5)) +
+    coord_flip()
 }
--- Shiny App/word_cloud.R
+++ Shiny App/word_cloud.R
@@ -1,17 +1,29 @@
-make_word_cloud = function(data){
-  words = data %>%
+word_cloud = function(data, sent = c("positive", "negative")){
+  data_sent = data %>%
+    inner_join(get_sentiments("bing"))
+  
+  if (sent == "positive"){
+    data_filt = data_sent %>%
+      filter(sentiment == "positive")
+  }else{
+    data_filt = data_sent %>%
+      filter(sentiment == "negative")
+  }
+  
+  words = data_filt %>%
     group_by(word) %>%
     count(word)
-
+  
   tidy_words <- words %>%
     anti_join(stop_words)
-
+  
   tidy_words = tidy_words %>%
     filter(word != "pic.twitter.com" &
              word != "https" &
              word != "http" &
-             word != "twitter.com")
-
+             word != "twitter.com" &
+             word != "trump")
+  
   wordcloud(words = tidy_words$word, freq = tidy_words$n, min.freq = 1,
             max.words=200, random.order=FALSE, rot.per=0.15,
             colors=brewer.pal(8, "Dark2"))
