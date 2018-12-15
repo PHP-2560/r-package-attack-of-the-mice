c81eb589eeb0cd82e13837275b710445544bb1af
Nicole Hardy <nicole_hardy@brown.edu>
2018-12-15 17:21:04 -0500

    Working app!

--- Over-time/app.R
+++ Over-time/app.R
@@ -53,7 +53,7 @@ ui <- fluidPage(navbarPage("Tabs",
                                     )
                            ),
   tabPanel("Over Time Tab",
-           titlePanel(h1("Over-Time Analysis of Twitter Data")),
+           shiny::titlePanel(h1("Over-Time Analysis of Twitter Data")),
            sidebarPanel(
                helpText("Explore how the sentiments change over time."),
                
@@ -63,7 +63,7 @@ ui <- fluidPage(navbarPage("Tabs",
                                           "Ariana Grande",
                                           "Chrissy Teigen")),
                
-               radioButtons(inputId = "lexiconChoice", 
+               radioButtons(inputId = "lexiconChoice2", 
                             label = "Choose a sentiment lexicon", 
                             choices = c("afinn", "bing", "nrc"),
                             selected = "afinn")
@@ -108,39 +108,39 @@ server <- function(input, output) {
   
   
   graph1 = reactive({
-    if(input$lexiconChoice == "afinn"){
+    if(input$lexiconChoice2 == "afinn"){
       source("afinn_avg_sentiment_per_day.R", local = TRUE)
       afinn_avg_sentiment_per_day(dataset(), dropwords = c("trump", "grand"))
-    } else if(input$lexiconChoice == "nrc"){
+    } else if(input$lexiconChoice2 == "nrc"){
       source("count_tweets_by_date.R", local = TRUE)
       count_tweet_by_date(dataset(), lexicon = "nrc", dropwords = c("trump", "grand"))
-    } else if(input$lexiconChoice == "bing"){
+    } else if(input$lexiconChoice2 == "bing"){
       source("count_tweets_by_date.R", local = TRUE)
       count_tweet_by_date(dataset(), lexicon = "bing", dropwords = c("trump", "grand"))
     }
   })
   
   graph2 = reactive({
-    if(input$lexiconChoice == "afinn"){
+    if(input$lexiconChoice2 == "afinn"){
       source("afinn_avg_sentiment_by_tweet.R", local = TRUE)
       afinn_avg_sentiment_per_tweet(dataset(), dropwords = c("trump", "grand"))
-    } else if(input$lexiconChoice == "nrc"){
+    } else if(input$lexiconChoice2 == "nrc"){
       source("tweet_percentage_by_date.R", local = TRUE)
       tweet_percentage_by_date(dataset(), lexicon = "nrc", dropwords = c("trump", "grand"))
-    } else if(input$lexiconChoice == "bing"){
+    } else if(input$lexiconChoice2 == "bing"){
       source("tweet_percentage_by_date.R", local = TRUE)
       tweet_percentage_by_date(dataset(), lexicon = "bing", dropwords = c("trump", "grand"))
     }
   })
   
   graph3 = reactive({
-    if(input$lexiconChoice == "afinn"){
+    if(input$lexiconChoice2 == "afinn"){
       source("num_tweets_per_day.R", local = TRUE)
       num_tweets_per_day(dataset(), lexicon = 'afinn', dropwords = c("trump", "grand"))
-    } else if(input$lexiconChoice == "nrc"){
+    } else if(input$lexiconChoice2 == "nrc"){
       source("num_tweets_per_day.R", local = TRUE)
       num_tweets_per_day(dataset(), lexicon = 'nrc', dropwords = c("trump", "grand"))
-    } else if(input$lexiconChoice == "bing"){
+    } else if(input$lexiconChoice2 == "bing"){
       source("num_tweets_per_day.R", local = TRUE)
       num_tweets_per_day(dataset(), lexicon = 'bing', dropwords = c("trump", "grand"))
     }
